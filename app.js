1f576a45db246813cb5b75828b63b4d67a179c98
udityaprakash <udityaprakash983@gmail.com>
2024-02-28 23:59:57 +0530

    create basic route of get

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const getroutes = require('./routes/getroutes');
 const app = express();
 const port = 3000;
 require('dotenv').config();
 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use('/', getroutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.all('*', (req, res) => {
+  res.status(404).json({error:true, message: 'Page not found'});
 });
 
 
