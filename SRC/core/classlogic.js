a9ecb4565772cabf28b3962526de2043ec0da7f7
udityaprakash <udityaprakash983@gmail.com>
2024-03-04 00:20:52 +0530

    create class api testing endpoint left

--- SRC/core/classlogic.js
+++ SRC/core/classlogic.js
@@ -6,16 +6,8 @@ const createClass =async (req, res)=>{
     try{
         const {classname, subject,section,description } = req.body;
 
-        const inputs = ['classname', 'subject', 'section', 'description'];
-
-        for (const key in inputs) {
-            
-            if (!req.body[key]) {
-                req.body[key] = '';
-            }
-        }
-        console.log(req.tokendata);
-        console.log(req.body.classname, req.body.subject, req.body.section, req.body.description, req.tokendata+"here");
+        console.log(req.tokendata.email);
+        // console.log(req.body.classname, req.body.subject, req.body.section, req.body.description, req.tokendata.email);
         let classid = uuidv4();
         let result =await bannerdb.find({}, { projection: { _id: 1 } });
         const ids = result.map(doc => doc._id);
@@ -23,10 +15,18 @@ const createClass =async (req, res)=>{
         if(ids.length < 5) res.status(200).json({error:true, message:"not enough banners to create class, please add more banners to the database"});
         
         const bannerid = ids[Math.floor(Math.random() * ids.length)];
-
+        console.log(bannerid);
         let cli = await client();
-        let response = await cli.run.query(`Insert into class (class_id,class_name, subject, section, description, email, banner_id) values($1,$2,$3,$4,$5,$6,$7);`,[classid,classname,subject,section,description,req.user.email,bannerid]);
-        console.log(ids.length + "bannerid: "+bannerid);
+        let response = await cli.run.query(`Insert into class (class_id,class_name, subject, section, description, email, banner_id) values($1,$2,$3,$4,$5,$6,$7);`,[
+            classid,
+            classname,
+            subject ? subject : '',
+            section ? section : '',
+            description ? description : '',
+            req.tokendata.email,
+            bannerid
+        ]);
+        console.log(response);
         res.status(200).json({error:false, message:"class created successfully"});
 
     }catch(e){
